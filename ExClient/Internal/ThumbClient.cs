fe3e6825fb620fa71c3096e8a41a388a47f30866
Minghao Hu <minghaohu@outlook.com>
2025-07-27 22:05:54 +0900

    Limit concurrency.

--- ExClient/Internal/ThumbClient.cs
+++ ExClient/Internal/ThumbClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using System.Threading.Tasks;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
@@ -16,9 +17,13 @@ namespace ExClient.Internal {
     internal static class ThumbClient {
         private static readonly HttpClient _Client = new();
 
+        private static readonly SemaphoreSlim _CacheLock = new SemaphoreSlim(1, 1);
         private static readonly PngEncoder _PngEncoder = new PngEncoder();
+        private static readonly int _MaxCacheSize = 800;
         private static readonly Queue<Uri> _KeyCache = new();
         private static readonly Dictionary<Uri, IBuffer> _Cache = new();
+
+        private static readonly SemaphoreSlim _ProcessingLock = new SemaphoreSlim(4, Math.Max(4, Environment.ProcessorCount));
 
         public static Uri FormatThumbUri(string uri) {
             if (uri.IsNullOrWhiteSpace())
@@ -42,7 +47,7 @@ namespace ExClient.Internal {
             try {
                 if (!TryGetCache(source, out var buf)) {
                     buf = await _Client.GetBufferAsync(source);
-                    PushCache(source, buf);
+                    await PushCache(source, buf);
                 }
                 using (var stream = buf.AsRandomAccessStream()) {
                     if (pageId == -1) {
@@ -58,51 +63,61 @@ namespace ExClient.Internal {
         }
 
         private static async Task CropThumbnailAsync(IRandomAccessStream stream, BitmapImage target, int pageId, int maxThumbnailCount = 20, int thumbnailWidth = 200) {
+            try {
+                await _ProcessingLock.WaitAsync();
+                using (Image image = await Image.LoadAsync(stream.AsStreamForRead())) {
+                    int totalThumbnails = image.Width / thumbnailWidth;
+                    int index = (pageId - 1) % maxThumbnailCount;
+                    int x = index * thumbnailWidth;
 
-            using (Image image = await Image.LoadAsync(stream.AsStreamForRead())) {
-                int totalThumbnails = image.Width / thumbnailWidth;
-                int index = (pageId - 1) % maxThumbnailCount;
-                int x = index * thumbnailWidth;
+                    await CropImageSync(image, new Rectangle(x, 0, thumbnailWidth, image.Height));
 
-                image.Mutate(ctx => ctx.Crop(
-                    new Rectangle(x, 0, thumbnailWidth, image.Height)));
+                    using (MemoryStream ms = new MemoryStream()) {
+                        await image.SaveAsync(ms, _PngEncoder);
+                        ms.Position = 0;
 
-                using (MemoryStream ms = new MemoryStream()) {
-                    await image.SaveAsync(ms, _PngEncoder);
-                    ms.Position = 0;
-
-                    target.SetSource(ms.AsRandomAccessStream());
+                        await target.SetSourceAsync(ms.AsRandomAccessStream());
+                    }
                 }
+            } finally {
+                _ProcessingLock.Release();
             }
         }
 
-        private static void PushCache(Uri uri, IBuffer buffer) {
-            lock (_Cache) {
+        private static async Task<Image> CropImageSync(Image image, Rectangle rect) {
+            await Task.Run(() => {
+                image.Mutate(ctx => ctx.Crop(rect));
+            });
+
+            return image;
+        }
+
+        private static async Task PushCache(Uri uri, IBuffer buffer) {
+            try {
+                await _CacheLock.WaitAsync();
                 if (_Cache.ContainsKey(uri)) {
                     _Cache[uri] = buffer;
                 } else {
-                    lock (_KeyCache) {
-                        if (_Cache.Count >= 20) {
-                            var key = _KeyCache.Dequeue();
-                            _Cache.Remove(key);
-                        }
-                        _KeyCache.Enqueue(uri);
-                        _Cache.Add(uri, buffer);
+                    if (_Cache.Count >= _MaxCacheSize) {
+                        var key = _KeyCache.Dequeue();
+                        _Cache.Remove(key);
                     }
+                    _KeyCache.Enqueue(uri);
+                    _Cache.Add(uri, buffer);
                 }
+            } finally {
+                _CacheLock.Release();
             }
         }
 
         private static bool TryGetCache(Uri uri, out IBuffer buffer) {
-            lock (_Cache) {
-                if (_Cache.ContainsKey(uri)) {
-                    buffer = _Cache[uri];
-                    return true;
-                }
-
-                buffer = null;
-                return false;
+            if (_Cache.ContainsKey(uri)) {
+                buffer = _Cache[uri];
+                return true;
             }
+
+            buffer = null;
+            return false;
         }
     }
 }
